20487e029b574c052b63fcd91404f164efe6aa85
fatooooon <coe240@hotmail.com>
2016-04-10 15:14:47 +0700

    #01
    สร้าง frame (กรอบแสดงผล)

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args){
+		JFrame frame = new JFrame("My App");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 650);
+		
+		frame.setVisible(true);
+	}
+}
